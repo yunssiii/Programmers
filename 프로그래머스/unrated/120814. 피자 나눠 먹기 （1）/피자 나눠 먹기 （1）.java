7bc946499758c171af092401febb3d57f0972d9a
황윤하 <120195100+yunssiii@users.noreply.github.com>
2023-11-20 16:52:06 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,19 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         
-        if(n<8){
-            answer = 1;
-            return answer;
-        }else {
-            
-            if(n%7 == 0){
-                answer = n/7;
-                return answer;
-            }else {
-                answer = n/7 + 1;
-            }
-        }
+        int answer = n%7==0 ? n/7:n/7+1;
         
         return answer;
     }
