3a5b33abd014dc392199094a2e6689c4f05d4248
황윤하 <120195100+yunssiii@users.noreply.github.com>
2023-11-20 20:18:30 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,20 +1,9 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
-        int coffee = 1;
-        int change = 0;
         
-        if(money<5500){
-            answer[0] = 0;
-            answer[1] = money;
-            return answer;
-        }else {
-            coffee = money/5500;
-            change = money-(5500*coffee);
-            
-            answer[0] = coffee;
-            answer[1] = change;
-        }
+        answer[0] = money/5500; //잔수
+        answer[1] = money%5500; //잔액
         
         return answer;
     }
