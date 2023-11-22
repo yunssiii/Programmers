1f9ef30dd35719c6569423da1c71ce271cd75c5d
황윤하 <120195100+yunssiii@users.noreply.github.com>
2023-11-22 16:27:56 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int num1 = 0;
+        int num2 = 0;
+        
+        //1. 짝수 %2==0 / 홀수 %2==1
+        
+        for(int i=0;i<num_list.length;i++){
+            if(num_list[i]%2==0){
+                num1++;
+            }else {
+                num2++;
+            }
+        }
+        
+        answer[0] = num1;
+        answer[1] = num2;
+        
+        
+        return answer;
+    }
+}
