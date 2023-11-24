2efac578017a3e7415534b3a1e305027952ea603
황윤하 <120195100+yunssiii@users.noreply.github.com>
2023-11-24 18:20:37 +0900

    [level unrated] Title: 개미 군단, Time: 0.03 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -4,11 +4,7 @@ class Solution {
         
         int num1 = hp/5; //장군개미의 수
         int num2 = (hp%5)/3; //병정개미의 수
-        int num3 = 0;
-
-        if((hp%5)%3>0){
-            num3 = (hp%5)%3; //일개미의 수 
-        }
+        int num3 = (hp%5)%3;
         
         answer = num1+num2+num3;
 
