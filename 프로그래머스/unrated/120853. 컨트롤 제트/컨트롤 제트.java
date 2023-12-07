4d8600161816812f3a28f2ad4d53fab34b19d013
황윤하 <120195100+yunssiii@users.noreply.github.com>
2023-12-07 17:00:44 +0900

    [level unrated] Title: 컨트롤 제트, Time: 0.41 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,19 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
         
-        //공백을 기준으로 나눠서 배열에 저장한다.
-        //배열의 길이만큼 int로 형변환해 더한다.
-        //z가 나오면 그 전 값을 뺀다.
+        //스택을 이용해보기
+        //우선 공백으로 문자열을 나눈 뒤 형변환해 스택에 저장한다.
+        //저장하다가 Z를 만나면 pop을 해서 최상단의 숫자를 뺀다.
+        //스택에 있는 정수들을 더해서 반환한다.
         
         int answer = 0;
-        String[] sArray = s.split(" ");
         
-        for(int i=0;i<sArray.length;i++){
-            if(sArray[i].equals("Z")){
-                answer -= Integer.parseInt(sArray[i-1]);
+        Stack<Integer> stack = new Stack<>();
+        
+        for(String x : s.split(" ")){
+            if(x.equals("Z")){
+                stack.pop();
             }else {
-                answer += Integer.parseInt(sArray[i]);
+                stack.push(Integer.parseInt(x));
             }
+        }
+        
+        for(int i : stack){
+            answer += i;
         }
         
         return answer;
