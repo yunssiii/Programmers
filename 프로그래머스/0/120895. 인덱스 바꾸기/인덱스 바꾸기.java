5682ec6cf1e5eb03f188288742054d14db8f7eab
황윤하 <120195100+yunssiii@users.noreply.github.com>
2024-01-28 18:03:09 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -2,17 +2,16 @@ class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
         
-        char c1 = my_string.charAt(num1);
-        char c2 = my_string.charAt(num2);
-        
-        StringBuilder stringBuilder = new StringBuilder(my_string);
-        
-        stringBuilder.setCharAt(num1,c2);
-        stringBuilder.setCharAt(num2,c1);
-        
+        String c1 = my_string.substring(num1, num1 + 1); //substring으로 해당 문자 구하기
+        String c2 = my_string.substring(num2, num2 + 1);
+
+        StringBuilder stringBuilder = new StringBuilder(my_string); //가변을 위해 StringBuilder 사용
+
+        stringBuilder.replace(num1, num1 + 1, c2); //num1 인덱스 자리 문자를 c2로 바꾸기
+        stringBuilder.replace(num2, num2 + 1, c1);
+
         answer = stringBuilder.toString();
         
         return answer;
     }
 }
-
