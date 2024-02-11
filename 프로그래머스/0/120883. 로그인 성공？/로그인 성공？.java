c25f10c39d2d3c88734a103a704d54356c1c77df
황윤하 <120195100+yunssiii@users.noreply.github.com>
2024-02-11 22:39:04 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail"; 
+        
+        for(int i=0; i<db.length; i++) {
+            if(id_pw[0].equals(db[i][0])) { // 아이디가 일치하는 경우
+                if(id_pw[1].equals(db[i][1])) { // 비밀번호도 일치하는 경우
+                    answer = "login";
+                    break; 
+                } else { // 비밀번호가 일치하지 않는 경우
+                    answer = "wrong pw";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
