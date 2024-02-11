bef95ea07eb88b3c4f39a7a1540d2d810371c19b
황윤하 <120195100+yunssiii@users.noreply.github.com>
2024-02-11 22:22:11 +0900

    [level 0] Title: 옹알이 (1), Time: 0.20 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,20 +3,14 @@ class Solution {
         int answer = 0;
         
         for(int i=0;i<babbling.length;i++){
-            if(babbling[i].contains("aya")){
-                babbling[i] = babbling[i].replace("aya"," ");
-            }
-            if(babbling[i].contains("ye")){
-                babbling[i] = babbling[i].replace("ye"," ");
-            }
-            if(babbling[i].contains("woo")){
-                babbling[i] = babbling[i].replace("woo"," ");
-            }
-            if(babbling[i].contains("ma")){
-                babbling[i] = babbling[i].replace("ma"," ");
-            }
             
-            if(babbling[i].equals(" ") || babbling[i].equals("  ") || babbling[i].equals("   ") || babbling[i].equals("    ")){
+                babbling[i] = babbling[i].replace("aya"," ")
+                                         .replace("ye"," ")
+                                         .replace("woo"," ")
+                                         .replace("ma"," ")
+                                         .replace(" ","");
+            
+            if(babbling[i].isEmpty()){
                 answer++;
             }
         }
