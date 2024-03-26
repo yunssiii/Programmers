5f38e3813b23e4b9ee43ef027a57bba4411cf321
황윤하 <120195100+yunssiii@users.noreply.github.com>
2024-03-26 21:25:27 +0900

    [level 2] Title: 동명 동물 수 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
+++ 프로그래머스/2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
@@ -1,2 +1,7 @@
 -- 코드를 입력하세요
-SELECT name, count(name) count from animal_ins group by name having count(name)>1 order by name; 
+SELECT name, count(name) 
+from animal_ins
+where name is not null
+group by name
+having count(name)>1
+order by name;
